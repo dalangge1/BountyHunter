151d1868a4e08bff67a50a14d74417b6a20d33c0
sandyz987 <1409618590@qq.com>
2020-12-26 17:16:51 +0800

    登录页面绘制

---
+++ app/src/main/java/com/example/bountyhunter/info/UserInfo.kt
@@ -0,0 +1,16 @@
+package com.example.bountyhunter.info
+
+/**
+ *@author zhangzhe
+ *@date 2020/12/26
+ *@description
+ */
+
+object UserInfo {
+    val nickname = ""
+    val password = ""
+    val token = ""
+    fun checkLogin(): Boolean {
+        return token.isEmpty() // TODO
+    }
+}
---
+++ app/src/main/java/com/example/bountyhunter/view/activity/ActivityMain.kt
@@ -0,0 +1,31 @@
+package com.example.bountyhunter.view.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bountyhunter.R
+import com.mredrock.cyxbs.common.utils.extensions.startActivityForResult
+
+class ActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivityForResult<ActivityLogin>(0)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            0 -> {
+
+            }
+            else -> {
+
+            }
+        }
+    }
+}
