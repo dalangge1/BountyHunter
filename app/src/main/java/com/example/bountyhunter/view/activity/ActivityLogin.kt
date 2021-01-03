13c717ba242607b795520609c05528d630bed582
sandyz987 <1409618590@qq.com>
2021-01-03 22:34:24 +0800

    基本信息配置

--- app/src/main/java/com/example/bountyhunter/view/activity/ActivityLogin.kt
+++ app/src/main/java/com/example/bountyhunter/view/activity/ActivityLogin.kt
@@ -3,10 +3,14 @@ package com.example.bountyhunter.view.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bountyhunter.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btn_register.setOnClickListener {
+            finish()
+        }
     }
 }
